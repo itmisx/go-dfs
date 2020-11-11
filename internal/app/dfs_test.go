12d72a6fa2a834bb8deb93c2c22b19679f39eda6
smally <smally84@126.com>
2020-11-11 14:33:38 +0800

    ✅  update test

--- internal/app/dfs_test.go
+++ internal/app/dfs_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/shirou/gopsutil/v3/disk"
 )
 
-func TestTracker(t *testing.T) {
+func TestDfs(t *testing.T) {
 	// 启动tracker
 	config1 := pkg.DsfConfigType{
 		ServerType:  "tracker",
